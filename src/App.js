eab819dfe36df34501845eef64ccce0f4d221beb
Vladyslav Krutyus <kijdron@gmail.com>
2024-05-29 16:47:39 +0300

    lab4-5

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+import Confetti from "./Confetti";
+
+const shareMessage = "I just ran my first container using Docker";
+const shareLink = "https://docker.com/";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Confetti />
+      <header className="App-header">
+        <h1 style={{ marginBottom: "0px" }}>Congratulations!!!</h1>
+        <p style={{ marginTop: "10px", marginBottom: "50px" }}>
+          You ran your first container.
+        </p>
+        <div>
+          <a
+            target="_blank"
+            href={
+              "https://twitter.com/intent/tweet?text=" +
+              shareMessage +
+              "&url=" +
+              shareLink
+            }
+            class="fa-brands fa-x-twitter"
+            rel="noopener noreferrer"
+          >
+            {" "}
+          </a>
+          {/*<a*/}
+          {/*  target="_blank"*/}
+          {/*  href={*/}
+          {/*    "https://reddit.com/submit?title=" +*/}
+          {/*    shareMessage +*/}
+          {/*    "&url=" +*/}
+          {/*    shareLink*/}
+          {/*  }*/}
+          {/*  class="fa-brands fa-reddit"*/}
+          {/*  rel="noopener noreferrer"*/}
+          {/*>*/}
+          {/*  {" "}*/}
+          {/*</a>*/}
+
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default App;
