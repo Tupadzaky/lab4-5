9d79e30ba85b96d4372d07d84fa8986d834793fc
Vladyslav Krutyus <kijdron@gmail.com>
2024-06-04 11:38:16 +0300

    deletion of a button

--- src/App.js
+++ src/App.js
@@ -27,32 +27,7 @@ const App = () => {
           >
             {" "}
           </a>
-          <a
-            target="_blank"
-            href={
-              "https://reddit.com/submit?title=" +
-              shareMessage +
-              "&url=" +
-              shareLink
-            }
-            class="fa-brands fa-reddit"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-          <a
-              target="_blank"
-              href={
-                  "https://reddit.com/submit?title=" +
-                  shareMessage +
-                  "&url=" +
-                  shareLink
-              }
-              className="fa-brands fa-reddit"
-              rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
+
 
         </div>
       </header>
