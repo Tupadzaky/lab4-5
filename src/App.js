66d16145839d01af3335d9c719d7bf6f8f2e1947
Vladyslav Krutyus <kijdron@gmail.com>
2024-06-04 11:36:49 +0300

    addition of a button

--- src/App.js
+++ src/App.js
@@ -40,6 +40,19 @@ const App = () => {
           >
             {" "}
           </a>
+          <a
+              target="_blank"
+              href={
+                  "https://reddit.com/submit?title=" +
+                  shareMessage +
+                  "&url=" +
+                  shareLink
+              }
+              className="fa-brands fa-reddit"
+              rel="noopener noreferrer"
+          >
+            {" "}
+          </a>
 
         </div>
       </header>
